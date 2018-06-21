eeea06dd8bf6d37467b66fcfb53ea8fa83318212
hubo36 <hubo11@jd.com>
2018-06-21 18:49:48 +0800

    深度截取300个作为参考

--- src/getSameAmount.js
+++ src/getSameAmount.js
@@ -4,6 +4,7 @@
  * @param {Array<Array<number>>} data 
  */
 const getSameAmount = function (data) {
+    data = data.slice(0, 300)
     let price = data[0][0];
     let countTemp = {};
     for (let i = 0; i < data.length; i++) {
@@ -18,24 +19,24 @@ const getSameAmount = function (data) {
     let arr = [];
     for (let key in countTemp) {
         
-        // 总价
-        let sumPrice = sum * price;
         // 同数量出现 的次数
         let count = countTemp[key];
         // 总量 = 次数 * 单个挂单量
         let sum = count * key;
+        // 总价
+        let sumPrice = sum * price;
 
-        if ((count > 1 && sumPrice > 500) || (key * price > 5000)) {
+        if ((count > 1 && sumPrice > 1000) || (key * price > 5000)) {
             arr.push({
                 'count': count,
                 '量': Number(key).toFixed(3),
                 s: sum.toFixed(3),
-                sp: '$' + (sumPrice| 0),
+                sp: (sumPrice| 0),
             });
         }
     }
     return arr.sort(function (a, b) {
-        return b.count - a.count
+        return b.sp - a.sp
     });
 }
 
