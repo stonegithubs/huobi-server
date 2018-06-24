00bab1e9b7e5d064bdc7a10f4afb3b800cf7023e
751968840@qq.com <751968840@qq.com>
2018-06-25 01:09:27 +0800

    Added ws

--- app.js
+++ app.js
@@ -3,69 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const wsinit = require('./lib/ws');
-
+// const wsinit = require('./lib/ws');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 // wsinit();
-// var app = express();
+var app = express();
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-const http = require('http');
-const WebSocket = require('ws');
- 
-const server = http.createServer();
-const wss1 = new WebSocket.Server({ noServer: true });
-const wss2 = new WebSocket.Server({ noServer: true });
- 
-wss1.on('connection', function connection(ws) {
-  console.log(ws)
-  // ...
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
- 
-wss2.on('connection', function connection(ws) {
-  // ...
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
- 
-server.on('upgrade', function upgrade(request, socket, head) {
-  const pathname = request.url;
- console.log(pathname)
-  if (pathname === '/foo') {
-    wss1.handleUpgrade(request, socket, head, function done(ws) {
-      wss1.emit('connection', ws, request);
-    });
-  } else if (pathname === '/bar') {
-    wss2.handleUpgrade(request, socket, head, function done(ws) {
-      wss2.emit('connection', ws, request);
-    });
-  } else {
-    socket.destroy();
-  }
-});
- 
-server.listen(3001);
-// module.exports = app;
+
+
+module.exports = app;
