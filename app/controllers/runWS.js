bca3627106fbd78d84062cb99346f4ca36baa10d
hubo36 <hubo11@jd.com>
2018-11-08 17:27:33 +0800

    Fix: catch没有传funciton

--- app/controllers/runWS.js
+++ app/controllers/runWS.js
@@ -1,6 +1,6 @@
 
 const WS_HUOBI = require('../../lib/ws-huobi');
-const WS_BINANCE = require('../../lib/ws-binance');
+// const WS_BINANCE = require('../../lib/ws-binance');
 const hbsdk = require('../../lib/sdk/hbsdk');
 const huobiSymbols = require('../utils/huobiSymbols');
 const { getWatchSymbols } = require('../models/charts');
@@ -16,14 +16,14 @@ async function start() {
             period: '5min',
         }).then((data) => {
             appConfig.prices.btc = data[1].close;
-        }).catch(console)
+        }).catch(console.error)
         ,
         hbsdk.getKline({
             symbol: 'ethusdt',
             period: '5min',
         }).then((data) => {
             appConfig.prices.eth = data[1].close;
-        }).catch(console)
+        }).catch(console.error)
         ,
         // 获取全部交易对的精度
         huobiSymbols.getSymbols()
@@ -32,29 +32,29 @@ async function start() {
             symbols = mysqlRes.map(item => item.symbol);
         })
     ]);
-    // await WS_HUOBI.open().then(function () {
-    //     symbols.forEach((symbol) => {
-    //         // 开始订阅
-    //         WS_HUOBI.subscribe.sub(WS_HUOBI, {
-    //             type: `sub`,
-    //             value: `market.${symbol}.depth.step0`,
-    //             symbol: `${symbol}`,
-    //             from: 'server'
-    //         });
-    //         WS_HUOBI.subscribe.sub(WS_HUOBI, {
-    //             type: `sub`,
-    //             value: `market.${symbol}.trade.detail`,
-    //             symbol: `${symbol}`,
-    //             from: 'server'
-    //         });
-    //         WS_HUOBI.subscribe.sub(WS_HUOBI, {
-    //             type: `sub`,
-    //             value:  `market.${symbol}.kline.1min`,
-    //             symbol: `${symbol}`,
-    //             from: 'server'
-    //         });
-    //     });
-    // });
+    await WS_HUOBI.open().then(function () {
+        symbols.forEach((symbol) => {
+            // 开始订阅
+            WS_HUOBI.subscribe.sub(WS_HUOBI, {
+                type: `sub`,
+                value: `market.${symbol}.depth.step0`,
+                symbol: `${symbol}`,
+                from: 'server'
+            });
+            WS_HUOBI.subscribe.sub(WS_HUOBI, {
+                type: `sub`,
+                value: `market.${symbol}.trade.detail`,
+                symbol: `${symbol}`,
+                from: 'server'
+            });
+            WS_HUOBI.subscribe.sub(WS_HUOBI, {
+                type: `sub`,
+                value:  `market.${symbol}.kline.1min`,
+                symbol: `${symbol}`,
+                from: 'server'
+            });
+        });
+    });
     // await WS_BINANCE.open();
 }
 exports.start = start;
