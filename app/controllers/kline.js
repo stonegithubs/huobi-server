004c60e63217f4bffbd924a6a4a91a3855677280
hubo36 <hubo36@jd.com>
2018-11-14 00:48:18 +0800

    MA计算

--- app/controllers/kline.js
+++ app/controllers/kline.js
@@ -1,22 +1,31 @@
 
 const AbnormalMonitor = require('../utils/AbnormalMonitor');
 const hbsdk = require('../../lib/sdk/hbsdk');
+const { CalculateMA } = require('../utils/calculateMA');
 const appConfig = require('../config');
 
 let pRange = 0.024;
 let aRage = 2.49;
 let heng = 0;
-const priceKline = new AbnormalMonitor({config: {range: pRange, disTime: 0, recordMaxLen: 300}});
-const amountKline = new AbnormalMonitor({config: {range: aRage, disTime: 0, recordMaxLen: 300}});
+let dataCount = 300;
+const priceKline = new AbnormalMonitor({config: {range: pRange, disTime: 0, recordMaxLen: dataCount}});
+const amountKline = new AbnormalMonitor({config: {range: aRage, disTime: 0, recordMaxLen: dataCount}});
 
 
 function train() {
     hbsdk.getKline({
         symbol: 'bchusdt',
         period: '30min',
-        size: 300,
+        size: dataCount,
     }).then((data) => {
-        data.reverse().forEach((item) => {
+        let MA5 = new CalculateMA(5);
+        let MA30 = new CalculateMA(30);
+        let MA60 = new CalculateMA(60);
+        let rData = data.reverse();
+        rData.forEach((item, index) => {
+            MA5.push(rData, index);
+            MA30.push(rData, index);
+            MA60.push(rData, index);
             priceKline.speed({
                 value: item.close,
                 ts: new Date(Number(item.id + '000')).getTime(),
@@ -35,16 +44,16 @@ function train() {
             let last2 = amountKline.historyStatus[amountKline.historyStatus.length - 1];
             // console.log()
             if (preLast2.status === '涨') {
-                if (preLast1.status === '涨') {
+                if (preLast1.status === '涨' || item.close > MA60.last()) {
                     console.log('卖', new Date(Number(item.id + '000')), (item.close * 1.01).toFixed(1));
                 }
-                if (preLast1.status === '跌') {
+                if (preLast1.status === '跌' && item.close < MA5.last()) {
                     console.log('买', new Date(Number(item.id + '000')), (item.close * (1 - 0.02)).toFixed(1));
                 }
             }
            
         });
-        console.log(data, priceKline, amountKline)
+        console.log(data, priceKline, amountKline, MA60)
     }).catch(console.error)
 }
 module.exports = train;
---
+++ app/utils/calculateMA.js
@@ -0,0 +1,49 @@
+
+/**
+ * 
+ * @param {number} dayCount 
+ * @param {*} data 
+ */
+function calculateMA(dayCount, data) {
+    var result = [];
+    for (var i = 0, len = data.length; i < len; i++) {
+        if (i < dayCount) {
+            result.push('-');
+            continue;
+        }
+        var sum = 0;
+        for (var j = 0; j < dayCount; j++) {
+            sum += data[i - j][1];
+        }
+        result.push(sum / dayCount);
+    }
+    return result;
+}
+
+
+exports.calculateMA = calculateMA;
+
+/**
+ * 与上面的功能一样，区别是此方法用于for遍历中，上面的已经把循环给做了
+ */
+class CalculateMA {
+    constructor(dayCount) {
+        this.dayCount = dayCount;
+        this.result = [];
+    }
+    push(data, index) {
+        if (index < this.dayCount) {
+            this.result.push('-');
+            return;
+        }
+        var sum = 0;
+        for (var j = 0; j < this.dayCount; j++) {
+            sum += data[index - j].close;
+        }
+        this.result.push(sum / this.dayCount);
+    }   
+    last() {
+        return this.result[this.result.length - 1];
+    }
+}
+exports.CalculateMA = CalculateMA;
